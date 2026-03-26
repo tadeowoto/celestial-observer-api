954f9a8253c0c9bef0c9fee53a45869a5cbe2b09
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-26 10:11:31 -0300

    BREAKING CHANGES: main structure added

---
+++ src/test/java/com/celestialobserver/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celestialobserver.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
