05a9dae6ac7d38b6bee02d40f99583cfbe1dfb70
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-26 12:17:01 -0300

    feat: get post by id endpoint added

--- src/main/java/com/celestialobserver/api/controller/PostController.java
+++ src/main/java/com/celestialobserver/api/controller/PostController.java
@@ -3,14 +3,13 @@ package com.celestialobserver.api.controller;
 import com.celestialobserver.api.entity.Post;
 import com.celestialobserver.api.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,12 +18,21 @@ public class PostController {
 
      private final PostService postService;
 
+
+
     @GetMapping("/{id}")
+    public ResponseEntity<Optional<Post>> getPostById(@PathVariable long id){
+        return ResponseEntity.ok(postService.findByPostId(id));
+    }
+
+
+
+    @GetMapping("/user/{id}")
     public ResponseEntity<List<Post>> getPostByUserId(@PathVariable long id){
         List<Post> posts = postService.listPostByUserId(id);
 
         return ResponseEntity.ok(posts);
-
     }
+
 
 }
--- src/main/java/com/celestialobserver/api/repository/PostRepository.java
+++ src/main/java/com/celestialobserver/api/repository/PostRepository.java
@@ -2,7 +2,6 @@ package com.celestialobserver.api.repository;
 
 import com.celestialobserver.api.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
